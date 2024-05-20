238ada7ffdf4dffef8d947fb15af35eb8dd88949
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2024-05-19 19:27:54 -0700

    gm

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,22 @@
 
 let main_button = false;
 
+function toggleMain() {
+    main_button = !main_button;
+    // alert(1)
+}
+
 </script>
 
 <svelte:window
-    on:keydown={main_button = true}
-    on:keyup={main_button = false}
+    on:keydown={toggleMain}
+    on:keyup={toggleMain}
+    style="{main_button ? 'background-color: red' : 'background-color: blue'}"
 />
 
+
+<input
+on:keydown={main_button = true}
+on:keyup={main_button = false}
+style="{main_button ? 'background-color: red' : 'background-color: blue'}"
+/>
