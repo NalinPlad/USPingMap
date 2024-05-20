8ed2c299f5ef53018d429ef19119484a1abfae2a
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2024-05-19 19:27:52 -0700

    gm

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+let main_button = false;
+
+</script>
+
+<svelte:window
+    on:keydown={main_button = true}
+    on:keyup={main_button = false}
+/>
+
